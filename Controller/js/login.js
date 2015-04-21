19667809e854c97acce5dde8789d3d7960209c48
rad1317 <radoslaw.pabisz@gmail.com>
2015-04-21 19:44:06 +0200

    Wstępne logowanie. Aby się wylogować usuń ciasteczka

---
+++ Controller/js/login.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    $("input#login").click(function(){ //zdarzenie obslugujące przycisk LOGIN
+		var nazwaUzytkownika=$("#nazwaUzytkownika").val();
+		var haslo=$("#haslo").val();
+		if(nazwaUzytkownika.length!=0 && haslo.length!=0)
+		{
+			login(nazwaUzytkownika,haslo);
+		}
+	});
+	$("button#logout").click(function(){ //wylogowanie sie
+		window.open('Controller/php/logout.php','_self');
+	})
+});
+function login(user,password){
+			$.ajax({
+				url:'Controller/php/login.php',
+				dataType:'json',
+				type:'POST',
+				data:{
+					"login":"login",
+					"nazwaUzytkownika":user,
+					"haslo":password
+					},
+				success : function(json) {
+           				 if(json["status"]==200)
+						 {
+							 window.open('index.php','_self');
+						 }
+						 else
+						 {
+							 $(".input_form").css({ border:"#FF0033 solid 2px"});
+							 alert("Nie udalo sie :(");
+						 }
+				},
+				error : function(err) {
+					alert("Blad");
+					}		
+			})
+}
---
+++ head.php
@@ -0,0 +1,8 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+       <head>
+       <title>Login</title>
+       <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+       <script src="Controller/js/jquery.js" type="text/javascript" ></script>
+       <script src="Controller/js/login.js" type="text/javascript" ></script>
+       </head>

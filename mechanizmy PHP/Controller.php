c58627c5c11b7314869a449bb8d5ff332d91ae86
Dominik Piegdoń <domi.pieg@gmail.com>
2015-04-25 23:38:33 +0200

    Zmiana Containera + Wylogowanie

---
+++ mechanizmy PHP/Controller.php
@@ -0,0 +1,52 @@
+﻿<?php
+require_once "./View/Gui.php";
+class Controller{
+	const quest=1;
+	const userStudent=2;
+	protected function user()
+	{
+		if(isset($_SESSION['userProjekt']))
+		{
+			return self::userStudent;
+		}
+		else
+		{
+			return self::quest;
+		}
+	}
+	public function load()
+	{
+		
+		$user=$this->user();
+		$gui= new Gui();
+		switch($user)
+		{
+			case self::quest:
+			$gui->setHead("View/head.html");
+			$gui->setMenu("View/menu.html");
+			if(!empty($_POST['nowekonto'])=="nowekonto")               //ło tutej
+			{															  //ło tutej	
+				$gui->setContenier("View/contenierRejestracji.html");        //ło tutej
+			} 
+			else{
+				$gui->setContenier("View/contenierLogowania.html");
+			}																  //ło tutej
+			$gui->setFooter("View/footer.html"); 
+			
+			
+			break;
+			case self::userStudent:
+			$gui->setHead("View/zalogowanie.html");
+			if(isset($_POST['wyloguj']))               //ło tutej
+			{															  //ło tutej	
+				session_destroy();
+				header('refresh: 0.01;');
+			} 
+			break;
+			
+		}
+		
+		$gui->showGui();
+	}
+}
+?>

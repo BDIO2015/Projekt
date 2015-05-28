cb75746fd18f478e465c2a4ed024243f676e7e53
bartoszpalka <bartosz.palka+github@deveo.pl>
2015-05-28 22:25:18 +0200

    New POST API function: addAttachment

--- webAPI/index.php
+++ webAPI/index.php
@@ -84,6 +84,7 @@ switch($method)
 	case "deleteComment": $result = $db->getThreadController()->deleteComment(); break;
 	case "getThread": $result = $db->getThreadController()->getThread(); break;
 	case "getThreads": $result = $db->getThreadController()->getThreads(); break;
+	case "addAttachment": $result = $db->getThreadController()->addAttachment(); break;
 
 	//otherController
 	case "getDepartments": $result = $db->getOtherController()->getDepartments(); break;
